316ab88682be1d139067e12e37ab8bbd95da95ab
smccar25 <smccarthy942@googlemail.com>
2019-03-03 13:08:25 +0000

    CC03
    PawnBP and C++ linked
    PC_BP and C++ linked
    GM_BP and C++ linked
    GI_BP and C++ linked

---
+++ CircuitCreatorV1/Source/CircuitCreatorV1/Public/Circuit_GI.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "Circuit_GI.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CIRCUITCREATORV1_API UCircuit_GI : public UGameInstance
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
---
+++ CircuitCreatorV1/Source/CircuitCreatorV1/Public/Circuit_PC.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "Circuit_PC.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CIRCUITCREATORV1_API ACircuit_PC : public APlayerController
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
